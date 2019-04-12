347a41ad12daeca9e74508326f27671db4f31cec
debbysa <debbysa@github.com>
2019-04-13 06:20:00 +0700

    perbaruhi tampilan dan fungsionalitas

--- app/src/main/java/com/github/debbysa/MainActivity.java
+++ app/src/main/java/com/github/debbysa/MainActivity.java
@@ -7,26 +7,29 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    public static final String EXTRA_MESSAGE = "com.github.debbysa.extra.MESSAGE";
-    private EditText mMessageEditText;
+    private EditText mMessageEditTextNama, mMessageEditTextAlamat, mMessageEditTextNohp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMessageEditText = findViewById(R.id.editText_main);
+        mMessageEditTextNama = findViewById(R.id.editText_nama);
+        mMessageEditTextAlamat = findViewById(R.id.editText_alamat);
+        mMessageEditTextNohp= findViewById(R.id.editText_nohp);
     }
 
     public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button diklik!");
+        Log.d(LOG_TAG, "Registrasi");
         Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
 
+        intent.putExtra("nama", mMessageEditTextNama.getText().toString());
+        intent.putExtra("alamat", mMessageEditTextAlamat.getText().toString());
+        intent.putExtra("nohp", mMessageEditTextNohp.getText().toString());
         startActivity(intent);
     }
 }
--- app/src/main/java/com/github/debbysa/SecondActivity.java
+++ app/src/main/java/com/github/debbysa/SecondActivity.java
@@ -3,20 +3,32 @@ package com.github.debbysa;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    String nama, alamat, nohp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        TextView text_message = findViewById(R.id.text_message);
         Intent intent = getIntent();
+        nama = intent.getStringExtra("nama");
+        alamat = intent.getStringExtra("alamat");
+        nohp = intent.getStringExtra("nohp");
 
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        text_message.setText(String.format("Nama : %s\nAlamat : %s\nNo Telepon : %s", nama, alamat, nohp));
+    }
 
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
+    public void launch_share(View view) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Nama : "+nama+ "\nAlamat : "+alamat+ "\nNo Telepon : "+nohp);
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
     }
 }
